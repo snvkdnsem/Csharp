7a685fe68264dbf36fcd1e5cd611182dc18b5344
JH <snvkdnsem@naver.com>
2018-10-30 18:40:06 +0900

    WPF 20181030

---
+++ 20181030/MvvmExam/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+namespace MvvmExam
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+            //System.Diagnostics.Process.GetCurrentProcess().Kill();
+            //this.Close();
+        }
+    }
+}
---
+++ 20181030/PropertyTrigger3/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Threading;
+
+namespace PropertyTrigger3
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void ButtonOnClick(object sender, RoutedEventArgs args)
+        {
+            Thread.Sleep(1000);
+            Button btn = args.Source as Button;
+            MessageBox.Show(btn.Content + " has been clicked", Title);
+        }    }
+}
